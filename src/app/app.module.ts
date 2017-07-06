edc3de9f87f27e95428da677a04898a81ea0090d
Denis Kamenskiy <kengji45@gmail.com>
2017-07-06 20:14:06 +0300

    add basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SignInComponent } from './sign-in/sign-in.component';
@@ -14,7 +15,13 @@ import { SignInComponent } from './sign-in/sign-in.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'sign-in',
+        component: SignInComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
